8222628b4eb20dc7023496c7bf5cd1f85996cc54
teaperr <teablake2006@gmail.com>
2024-09-25 17:35:28 +0100

    Wed 25 Sep 17:35:28 BST 2024

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,6 +25,24 @@
     xclip
     zsh-autosuggestions
     zsh-syntax-highlighting
+    neovim
+    tree
+    librewolf
+    tmux
+    vesktop
+    xorg.xinput
+    xclip
+    btop
+    htop
+    catppuccin
+    rofi
+    tint2
+    picom
+    spaceFM
+    gnome-settings-daemon
+    volumeicon
+    dunst
+    obs-studio
   ];
 
   # Home Manager version and other configurations
