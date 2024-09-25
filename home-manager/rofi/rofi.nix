4b483fe885c53a929b2798ba7a838626bab4243c
teaperr <teablake2006@gmail.com>
2024-09-25 20:37:17 +0100

    Wed 25 Sep 20:37:17 BST 2024

---
+++ home-manager/rofi/rofi.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  home.file.".config/rofi/rofi.rasi".text ''
+configuration{
+    modi: "run,drun,window";
+    icon-theme: "Oranchelo";
+    show-icons: true;
+    terminal: "alacritty";
+    drun-display-format: "{icon} {name}";
+    location: 0;
+    disable-history: false;
+    hide-scrollbar: true;
+    display-drun: "   Apps ";
+    display-run: "   Run ";
+    display-window: " 󰕰  Window";
+    display-Network: " 󰤨  Network";
+    sidebar-mode: true;
+    dpi: 70;
+}
+
+@theme "catppuccin-mocha"
+
+window {
+    width: 40%;
+    height: 40%;
+}
+  '';
+
+  home.file.".config/rofi/userconfig/config.rasi".text ''
+configuration{
+    modi: "run,drun,window";
+    icon-theme: "Oranchelo";
+    show-icons: true;
+    terminal: "alacritty";
+    drun-display-format: "{icon} {name}";
+    location: 0;
+    disable-history: false;
+    hide-scrollbar: true;
+    display-drun: "   Apps ";
+    display-run: "   Run ";
+    display-window: " 󰕰  Window";
+    display-Network: " 󰤨  Network";
+    sidebar-mode: true;
+}
+
+@theme "catppuccin-mocha"
+  '';
+}
